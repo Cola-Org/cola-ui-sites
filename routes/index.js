b07b26cec53e7283886c23f9c111eaf0535bb6cf
Alex Tong <alex.tong007@gmail.com>
2017-05-03 23:46:53 +0800

    美化文档框架

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const router = express.Router();
-const articles = require("./data/docs-menus")
+var express = require('express');
+var router = express.Router();
+var articles = require("./data/docs-menus")
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', function (req, res, next) {
 	res.render('index', {title: 'Express', articles: articles});
 });
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', function (req, res, next) {
 	res.render('index', {title: 'Express'});
 });
 
